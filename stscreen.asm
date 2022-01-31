5e39825c76f8d0bda52712e0380e846983af7c2a
Nemonet <79866006+The-Young-Programmer@users.noreply.github.com>
2022-01-31 14:15:17 +0100

    Create stscreen.asm

---
+++ stscreen.asm
@@ -0,0 +1,67 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;      __   __  _______  __    _  _______  __   __  _______  __    _          ;;
+;;     |  | |  ||   _   ||  |  | ||       ||  |_|  ||   _   ||  |  | |         ;;
+;;     |  |_|  ||  |_|  ||   |_| ||    ___||       ||  |_|  ||   |_| |         ;;
+;;     |       ||       ||       ||   | __ |       ||       ||       |         ;;
+;;     |       ||       ||  _    ||   ||  ||       ||       ||  _    |         ;;
+;;     |   _   ||   _   || | |   ||   |_| || ||_|| ||   _   || | |   |         ;;
+;;     |__| |__||__| |__||_|  |__||_______||_|   |_||__| |__||_|  |__|         ;;
+;;                                                                             ;;
+;;                                                                             ;;
+;;  HANGMAN - An implementation of the Hang Man game in assembly (Emu8086)     ;;
+;;                                                                             ;;
+;;  Created by The Young Programmer [TYP 💻] in 2022                           ;;
+;;  Created by The Young Programmer [TYP 💻] in 2022                           ;;
+;;  Created by The Young Programmer [TYP 💻] in 2022                           ;;
+;;  Created by The Young Programmer [TYP 💻] in 2022                           ;;
+;;                                                                             ;;           
+;;                                                                             ;;
+;;  HangMan game is free software: I value keeping my site open source,        ;;
+;;  but as you all know, PLAGIARISM IS BAD. It's always disheartening          ;;
+;;  whenever I find that someone has copied my code without giving me credit   ;;
+;;  I spent a lot of time and effort building this game.                       ;;
+;;  All I ask of you all is to not claim this effort as your own.              ;;
+;;  You can redistribute it and/or modify.                                     ;;
+;;                                                                             ;;
+;;            Please give me proper credit by linking back to                  ;;
+;;     https://github.com/The-Young-Programmer/Hangman-_-ASM. Thanks!          ;;
+;;                                                                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;
+;; Contains the function that prints the startup screen.
+;;
+;; Index:
+;;     _print_startup_screen() -- Prints the startup screen.
+;;
+
+
+
+;================================================ _print_startup_screen() ====
+;; Prints the startup screen.
+
+;; Usage:
+;; call _print_startup_screen
+
+
+_print_startup_screen:
+
+call _clear_screen
+
+mov POS_X, (COLS-startup_scr_len)/2
+mov POS_Y, ROWS-startup_scr_height-2
+
+mov ah, 0x09
+mov dx, offset startup_scr
+
+;Header
+prn_st_scr_loop:
+    call _move_cursor
+    int 0x21
+    inc POS_Y
+    add dx, startup_scr_len
+    cmp POS_Y, startup_scr_height+(ROWS-startup_scr_height-2)
+    jne prn_st_scr_loop
+
+ret
+
